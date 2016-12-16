8f8869cd013266d47661721aa155181c843776f8
danielmarbach <daniel.marbach@openplace.net>
2016-12-16 10:29:05 +0100

    More tests, Api approvals

--- src/NServiceBus.Callback.Testing.Tests/TestablecallbackAwareSessionTests.cs
+++ src/NServiceBus.Callback.Testing.Tests/TestablecallbackAwareSessionTests.cs
@@ -20,12 +20,43 @@
         }
 
         [Test]
+        public void When_request_option_matcher_matches_with_wrong_response_type()
+        {
+            var callbackSession = new TestableCallbackAwareSession();
+            var expectedHeader = "TestingHeader";
+
+            callbackSession.When<Request, int>((r, o) => r.MagicNumber == 42 && o.GetHeaders().ContainsKey(expectedHeader), 42);
+
+            var options = new SendOptions();
+            options.SetHeader(expectedHeader, "value");
+
+            var exception = Assert.ThrowsAsync<InvalidOperationException>(async () => await callbackSession.Request<string>(new Request { MagicNumber = 42 }, options));
+            StringAssert.Contains("Matcher matched but response type 'System.Int32' is incompatible with expected response type of 'System.String'.", exception.Message);
+        }
+
+        [Test]
         public async Task When_request_matcher_matches_should_return_response()
         {
             var callbackSession = new TestableCallbackAwareSession();
             callbackSession.When<Request, string>(r => r.MagicNumber == 42, "HelloWorld");
 
             var result = await callbackSession.Request<string>(new Request { MagicNumber = 42 });
+
+            Assert.AreEqual("HelloWorld", result);
+        }
+
+        [Test]
+        public async Task When_request_option_matcher_matches_should_return_response()
+        {
+            var callbackSession = new TestableCallbackAwareSession();
+            var expectedHeader = "TestingHeader";
+
+            callbackSession.When<Request, string>((r, o) => r.MagicNumber == 42 && o.GetHeaders().ContainsKey(expectedHeader), "HelloWorld");
+
+            var options = new SendOptions();
+            options.SetHeader(expectedHeader, "value");
+
+            var result = await callbackSession.Request<string>(new Request { MagicNumber = 42 }, options);
 
             Assert.AreEqual("HelloWorld", result);
         }
@@ -47,6 +78,26 @@
         }
 
         [Test]
+        public void When_request_option_matcher_does_not_match_should_honor_cancellation()
+        {
+            var tokenSource = new CancellationTokenSource();
+            var expectedHeader = "TestingHeader";
+
+            var callbackSession = new TestableCallbackAwareSession();
+
+            callbackSession.When<Request, string>((r, o) =>
+            {
+                tokenSource.Cancel();
+                return r.MagicNumber == 42 && o.GetHeaders().ContainsKey("WrongHeader");
+            }, "HelloWorld");
+
+            var options = new SendOptions();
+            options.SetHeader(expectedHeader, "value");
+
+            Assert.ThrowsAsync<TaskCanceledException>(async () => await callbackSession.Request<string>(new Request { MagicNumber = 42 }, options, tokenSource.Token));
+        }
+
+        [Test]
         public async Task When_request_matcher_matches_should_not_honor_cancellation()
         {
             var tokenSource = new CancellationTokenSource();
@@ -60,6 +111,28 @@
             }, "HelloWorld");
 
             var result = await callbackSession.Request<string>(new Request { MagicNumber = 42 }, tokenSource.Token);
+
+            Assert.AreEqual("HelloWorld", result);
+        }
+
+        [Test]
+        public async Task When_request_option_matcher_matches_should_not_honor_cancellation()
+        {
+            var tokenSource = new CancellationTokenSource();
+            var expectedHeader = "TestingHeader";
+
+            var callbackSession = new TestableCallbackAwareSession();
+
+            callbackSession.When<Request, string>((r,o) =>
+            {
+                tokenSource.Cancel();
+                return r.MagicNumber == 42 && o.GetHeaders().ContainsKey(expectedHeader);
+            }, "HelloWorld");
+
+            var options = new SendOptions();
+            options.SetHeader(expectedHeader, "value");
+
+            var result = await callbackSession.Request<string>(new Request { MagicNumber = 42 }, options, tokenSource.Token);
 
             Assert.AreEqual("HelloWorld", result);
         }
