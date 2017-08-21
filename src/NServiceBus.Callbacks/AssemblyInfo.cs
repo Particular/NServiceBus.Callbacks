de942184332400d58b3d758db1599c8a6b4d0779
andreasohlund <andreas.ohlund@particular.net>
2017-08-21 09:20:08 +0200

    Fix versioning

--- src/NServiceBus.Callbacks/AssemblyInfo.cs
+++ src/NServiceBus.Callbacks/AssemblyInfo.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NServiceBus.Callbacks")]
-[assembly: AssemblyCopyright("Copyright NServiceBus. All rights reserved")]
-[assembly: AssemblyProduct("NServiceBus.Callbacks")]
-[assembly: AssemblyCompany("NServiceBus Ltd.")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
