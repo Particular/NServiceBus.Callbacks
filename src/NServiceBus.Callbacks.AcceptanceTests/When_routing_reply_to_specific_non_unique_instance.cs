92704f59360b880bf65b150a90c967b3a6cee5fc
andreasohlund <andreas.ohlund@particular.net>
2017-10-26 12:56:03 +0200

    More tests

--- src/NServiceBus.Callbacks.AcceptanceTests/When_routing_reply_to_specific_non_unique_instance.cs
+++ src/NServiceBus.Callbacks.AcceptanceTests/When_routing_reply_to_specific_non_unique_instance.cs
@@ -6,17 +6,36 @@ namespace NServiceBus.AcceptanceTests.Callbacks
     using EndpointTemplates;
     using NUnit.Framework;
 
-    public class When_routing_reply_to_specific_instance : NServiceBusAcceptanceTest
+    public class When_routing_reply_to_specific_non_unique_instance : NServiceBusAcceptanceTest
     {
         [Test]
-        public void Should_throw_if_instance_is_not_uniquely_addressable()
+        public void Should_throw_if_requested_explicitly()
         {
             var ex = Assert.ThrowsAsync<InvalidOperationException>(async () => await Scenario.Define<Context>()
                 .WithEndpoint<EndpointWithNoUniqueAddress>(e => e.When(async (s, c) =>
                 {
                     var options = new SendOptions();
 
+                    options.RouteReplyToThisInstance();
                     options.RouteToThisEndpoint();
+
+                    await s.Request<MyResponse>(new MyRequest(), options);
+                }))
+                .Done(c => c.EndpointsStarted)
+                .Run());
+
+            StringAssert.Contains("MakeInstanceUniquelyAddressable", ex.Message);
+        }
+
+        [Test]
+        public void Should_throw_by_default()
+        {
+            var ex = Assert.ThrowsAsync<InvalidOperationException>(async () => await Scenario.Define<Context>()
+                .WithEndpoint<EndpointWithNoUniqueAddress>(e => e.When(async (s, c) =>
+                {
+                    var options = new SendOptions();
+
+                   options.RouteToThisEndpoint();
 
                     await s.Request<MyResponse>(new MyRequest(), options);
                 }))
