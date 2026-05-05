7bdba4e690ec057d966d93a8729d49e435dae59f
Tamara Rivera <tamita.rivera@gmail.com>
2026-05-04 17:31:47 -0700

    Change assertion to expect response to equal 200

--- src/NServiceBus.Callbacks.AcceptanceTests/When_using_int_response_and_conventions.cs
+++ src/NServiceBus.Callbacks.AcceptanceTests/When_using_int_response_and_conventions.cs
@@ -20,7 +20,7 @@
                 .Done(c => c.CallbackFired)
                 .Run();
 
-            Assert.That(context.Response, Is.Not.EqualTo(200));
+            Assert.That(context.Response, Is.EqualTo(200));
         }
 
         class Context : ScenarioContext
